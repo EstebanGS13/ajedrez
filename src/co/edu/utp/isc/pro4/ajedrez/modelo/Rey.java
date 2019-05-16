481a62b67ab3ab99847178b40f6c864d9e1c9ab4
Esteban <estebangsan13@gmail.com>
2019-05-15 22:45:06 -0500

    Agregando diseño de fichas: rey y dama

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Rey.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Rey.java
@@ -5,7 +5,11 @@
  */
 package co.edu.utp.isc.pro4.ajedrez.modelo;
 
+import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
 
 /**
  *
@@ -29,7 +33,35 @@ public class Rey extends Ficha {
 
     @Override
     public void draw(Graphics2D g, float x, float y) {
-        //TODO Dibujar la figura
+        GeneralPath polyline = new GeneralPath(GeneralPath.WIND_EVEN_ODD, 17);
+        polyline.moveTo(x + 18, y + 6);
+        polyline.lineTo(x + 18, y + 10);
+        polyline.lineTo(x + 23, y + 10);
+        polyline.lineTo(x + 23, y + 15);
+        polyline.lineTo(x + 27, y + 15);
+        polyline.lineTo(x + 27, y + 10);
+        polyline.lineTo(x + 32, y + 10);
+        polyline.lineTo(x + 32, y + 6);
+        polyline.lineTo(x + 27, y + 6);
+        polyline.lineTo(x + 27, y + 2);
+        polyline.lineTo(x + 23, y + 2);
+        polyline.lineTo(x + 23, y + 6);
+        polyline.lineTo(x + 18, y + 6);
+       
+        
+        
+        g.setPaint(new GradientPaint(x, y,
+                getColor() == Color.BLANCO ? java.awt.Color.CYAN : java.awt.Color.BLACK,    //color de la ficha
+                x + 50, y + 50,
+                java.awt.Color.WHITE));
+        g.fill(polyline);
+        g.draw(polyline);
+        g.fill(new Rectangle2D.Float(x + 15, y + 15, 20, 25));
+        g.fill(new Rectangle2D.Float(x + 10, y + 40, 30, 5));
+        g.setPaint(java.awt.Color.BLACK);
+        g.draw(new Rectangle2D.Float(x + 15, y + 15, 20, 25));
+        g.draw(new Rectangle2D.Float(x + 10, y + 40, 30, 5));
+       
     }
 
 }
