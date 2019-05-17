1083f48dd3ad67df73b99338255d03f22b14357c
Esteban <estebangsan13@gmail.com>
2019-05-17 13:22:28 -0500

    Diseño del alfil

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Alfil.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Alfil.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package co.edu.utp.isc.pro4.ajedrez.modelo;
 
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 
-/**
- *
- * @author utp
- */
+
 public class Alfil extends Ficha {
 
     public Alfil(Color color) {
@@ -31,7 +25,22 @@ public class Alfil extends Ficha {
 
     @Override
     public void draw(Graphics2D g, float x, float y) {
-        //TODO Dibujar la figura
+        GeneralPath polyline = new GeneralPath(GeneralPath.WIND_EVEN_ODD, 17);
+        polyline.moveTo(x + 22, y + 20);
+        polyline.lineTo(x + 15, y + 45);
+        polyline.lineTo(x + 35, y + 45);
+        polyline.lineTo(x + 28, y + 20);
+        polyline.lineTo(x + 22, y + 20);
+        
+        g.setPaint(new GradientPaint(x, y,
+                getColor() == Color.BLANCO ? java.awt.Color.CYAN : java.awt.Color.BLACK,    //color de la ficha
+                x + 50, y + 50,
+                java.awt.Color.WHITE));
+        g.fill(new Ellipse2D.Float(x + 18, y + 7, 13, 13));
+        g.fill(polyline);
+        
+        g.setColor(java.awt.Color.BLACK);
+        g.draw(new Ellipse2D.Float(x + 18, y + 7, 13, 13));
+        g.draw(polyline);
     }
-    
 }
