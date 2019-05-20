00d2d0174d54582c13a1274f1ef2d169c38059ce
Esteban <estebangsan13@gmail.com>
2019-05-19 21:23:09 -0500

    Diseño del caballo

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Caballo.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Caballo.java
@@ -3,8 +3,7 @@ package co.edu.utp.isc.pro4.ajedrez.modelo;
 
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
+import java.awt.geom.GeneralPath;
 
 
 public class Caballo extends Ficha {
@@ -25,7 +24,28 @@ public class Caballo extends Ficha {
 
     @Override
     public void draw(Graphics2D g, float x, float y) {
-        //TODO Dibujar la figura
+        GeneralPath polyline = new GeneralPath(GeneralPath.WIND_EVEN_ODD, 13);
+        polyline.moveTo(x + 10, y + 42);
+        polyline.lineTo(x + 26, y + 25);
+        polyline.lineTo(x + 19, y + 22);
+        polyline.lineTo(x + 16, y + 25);
+        polyline.lineTo(x + 13, y + 25);
+        polyline.lineTo(x + 10, y + 19);
+        polyline.lineTo(x + 24, y + 8);
+        polyline.lineTo(x + 24, y + 5);
+        polyline.lineTo(x + 26, y + 5);
+        polyline.lineTo(x + 38, y + 21);
+        polyline.lineTo(x + 40, y + 45);
+        polyline.lineTo(x + 12, y + 45);
+        polyline.lineTo(x + 10, y + 42);
+        
+        g.setPaint(new GradientPaint(x, y,
+                getColor() == Color.BLANCO ? java.awt.Color.CYAN : java.awt.Color.BLACK,
+                x + 100, y + 50,
+                java.awt.Color.WHITE));
+        g.fill(polyline);
 
+        g.setColor(java.awt.Color.BLACK);
+        g.draw(polyline);
     }
 }
