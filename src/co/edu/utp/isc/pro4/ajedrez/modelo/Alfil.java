6c5a0e7ddfafb38e799c964c3f1d3af1ef641613
Esteban <estebangsan13@gmail.com>
2019-05-25 11:17:47 -0500

    Movimiento del alfil

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Alfil.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Alfil.java
@@ -13,7 +13,69 @@ public class Alfil extends Ficha {
         super(color);
     }
 
-
+    @Override
+    public boolean validarMovimiento(Casilla casillaInicio, Casilla casillaFin, Color color, Tablero tablero) {
+        int colInicio = casillaInicio.getColumna() - 'A';
+        int filaInicio = casillaInicio.getFila() - 1;
+        
+        int colFin = casillaFin.getColumna() - 'A';
+        int filaFin = casillaFin.getFila() - 1;
+        
+        Casilla casillaIntermedia = null;
+        
+        if (colInicio > colFin && filaInicio > filaFin) {
+            System.out.println("noroeste");
+            for (int i = filaInicio-1, j = colInicio-1; 
+                    i >= filaFin && j >= colFin;
+                    i--, j--) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia.isOcupada()) {
+                        return false;
+                }
+            }
+        } else if (colInicio > colFin && filaInicio < filaFin) {
+                System.out.println("suroeste");
+            for (int i = filaInicio+1, j = colInicio-1; 
+                    i <= filaFin && j >= colFin;
+                    i++, j--) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia.isOcupada()) {
+                        return false;
+                }
+            }
+        } else if (colInicio < colFin && filaInicio > filaFin) {
+            System.out.println("noreste");
+            for (int i = filaInicio-1, j = colInicio+1; 
+                    i >= filaFin && j <= colFin;
+                    i--, j++) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia.isOcupada()) {
+                        return false;
+                }
+            }
+        } else if (colInicio < colFin && filaInicio < filaFin) {
+            System.out.println("sureste");
+            for (int i = filaInicio+1, j = colInicio+1; 
+                    i <= filaFin && j <= colFin;
+                    i++, j++) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia.isOcupada()) {
+                        return false;
+                }
+                System.out.println("sale");
+            }
+        }
+        if (casillaIntermedia == casillaFin) {
+            if (casillaFin.isOcupada()) {
+                this.comer();
+                return true;
+            }
+            this.mover(casillaInicio, casillaFin);
+            return true;
+        }
+        return false;
+    }
+    
     @Override
     public void comer() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -38,11 +100,5 @@ public class Alfil extends Ficha {
         g.setColor(java.awt.Color.BLACK);
         g.draw(new Ellipse2D.Float(x + 18, y + 7, 13, 13));
         g.draw(polyline);
-    }
-
-    @Override
-    public boolean validarMovimiento(Casilla casillaInicio, Casilla casillaFin, Color color, Tablero tablero) {
-        this.mover(casillaInicio, casillaFin);
-        return true;
-    }
+    } 
 }
