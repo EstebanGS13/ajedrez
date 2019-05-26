ca3f50247241f98d94c4f48d5133adbd68f53ff7
Esteban <estebangsan13@gmail.com>
2019-05-26 10:07:59 -0500

    Movimiento de la reina, juntando los movimientos de la torre y del alfil (provisional)

--- src/co/edu/utp/isc/pro4/ajedrez/modelo/Reina.java
+++ src/co/edu/utp/isc/pro4/ajedrez/modelo/Reina.java
@@ -13,6 +13,103 @@ public class Reina extends Ficha {
         super(color);
     }
 
+    @Override
+    public boolean validarMovimiento(Casilla casillaInicio, Casilla casillaFin, Color color, Tablero tablero) {
+        int colInicio = casillaInicio.getColumna() - 'A';
+        int filaInicio = casillaInicio.getFila() - 1;
+        
+        int colFin = casillaFin.getColumna() - 'A';
+        int filaFin = casillaFin.getFila() - 1;
+        
+        Casilla casillaIntermedia = null;
+        
+        if (colInicio == colFin) {              //misma columna
+            if (filaInicio  > filaFin) {        //mueve arriba
+                for (int i = filaInicio-1; i >= filaFin; i--) {
+                    casillaIntermedia = tablero.getCasilla(i, colFin);
+                    if (casillaIntermedia != casillaFin
+                            && casillaIntermedia.isOcupada()) {
+                        return false;
+                    }
+                }
+            } else if (filaInicio < filaFin) {  //mueve abajo
+                for (int i = filaInicio+1; i <= filaFin; i++) {
+                    casillaIntermedia = tablero.getCasilla(i, colFin);
+                    if (casillaIntermedia != casillaFin 
+                            && casillaIntermedia.isOcupada()) {
+                        return false;
+                    }
+                }
+            }
+        } else if (filaInicio == filaFin) {     //misma fila
+            if (colInicio > colFin) {           //mueve izquierda
+                for (int j = colInicio-1; j >= colFin; j--) {
+                    casillaIntermedia = tablero.getCasilla(filaFin, j);
+                    if (casillaIntermedia != casillaFin 
+                            && casillaIntermedia.isOcupada()) {
+                        return false;
+                    }
+                }
+            } else if (colInicio < colFin) {    //mueve derecha
+                for (int j = colInicio+1; j <= colFin; j++) {
+                    casillaIntermedia = tablero.getCasilla(filaFin, j);
+                    if (casillaIntermedia != casillaFin 
+                            && casillaIntermedia.isOcupada()) {
+                        return false;
+                    }
+                }
+            }
+        } else if (colInicio > colFin && filaInicio > filaFin) {           //noroeste
+            for (int i = filaInicio-1, j = colInicio-1; 
+                    i >= filaFin && j >= colFin;
+                    i--, j--) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia != casillaFin 
+                        && casillaIntermedia.isOcupada()) {
+                    return false;
+                }
+            }
+        } else if (colInicio > colFin && filaInicio < filaFin) {    //suroeste
+            for (int i = filaInicio+1, j = colInicio-1; 
+                    i <= filaFin && j >= colFin;
+                    i++, j--) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia != casillaFin 
+                        && casillaIntermedia.isOcupada()) {
+                    return false;
+                }
+            }
+        } else if (colInicio < colFin && filaInicio > filaFin) {    //noreste
+            for (int i = filaInicio-1, j = colInicio+1; 
+                    i >= filaFin && j <= colFin;
+                    i--, j++) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia != casillaFin 
+                        && casillaIntermedia.isOcupada()) {
+                    return false;
+                }
+            }
+        } else if (colInicio < colFin && filaInicio < filaFin) {    //sureste
+            for (int i = filaInicio+1, j = colInicio+1; 
+                    i <= filaFin && j <= colFin;
+                    i++, j++) {
+                casillaIntermedia = tablero.getCasilla(i, j);
+                if (casillaIntermedia != casillaFin 
+                        && casillaIntermedia.isOcupada()) {
+                    return false;
+                }
+            }
+        }
+        if (casillaIntermedia == casillaFin) {
+            if (casillaFin.isOcupada()) {
+                this.comer();
+                return true;
+            }
+            this.mover(casillaInicio, casillaFin);
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public void comer() {
@@ -32,11 +129,5 @@ public class Reina extends Ficha {
         g.draw(new Ellipse2D.Float(x + 17, y + 1, 15, 15));
         g.draw(new Rectangle2D.Float(x + 15, y + 15, 20, 25));
         g.draw(new Rectangle2D.Float(x + 10, y + 40, 30, 5));
-    }
-
-    @Override
-    public boolean validarMovimiento(Casilla casillaInicio, Casilla casillaFin, Color color, Tablero tablero) {
-        this.mover(casillaInicio, casillaFin);
-        return true;
     }
 }
